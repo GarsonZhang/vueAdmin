eb608ab467740108260484c38a3e28b587568d5d
GarsonZhang <garson_zhang@163.com>
2017-10-30 14:32:13 +0800

    模块管理布局设计

--- src/views/system/module/index.vue
+++ src/views/system/module/index.vue
@@ -2,36 +2,56 @@
 <div class="content">
   <Row class="fill-height">
     <Col span="6" class="fill-height">
-    <gz-panel class="borderPanel" >
-       <div slot="top" class="tools" >
-          <div class="title"><span>模块列表</span></div>
-       </div>
-      <div slot="main">
-        <gz-panel ref="gzabc" :childFull100=true >
-            <div slot="top" class="tools" >    
-              <Tooltip content="刷新" placement="bottom-start">
-                <gz-button ref="refresh" icon="refresh" @click="doRresh" :showText='false' />
-              </Tooltip>
-              <ButtonGroup class="tooltipButtonGroup">
-                <Tooltip content="新增" placement="bottom-start" >
-                  <Button type="primary" icon="plus" @click="doAdd"></Button>
+      <gz-panel class="borderPanel" >
+        <div slot="top" class="title" >
+            <span>模块列表</span>
+        </div>
+        <div slot="main">
+          <gz-panel ref="gzPanelModule" :childFull100=true >
+              <div slot="top" class="tools" >    
+                <Tooltip content="刷新" placement="bottom-start">
+                  <gz-button ref="refresh" icon="refresh" @click="doRresh" :showText='false' />
                 </Tooltip>
-                <Tooltip content="修改" placement="bottom-start" >
-                  <Button type="success" icon="edit"></Button>
+                <ButtonGroup class="tooltipButtonGroup">
+                  <Tooltip content="新增" placement="bottom-start" >
+                    <Button type="primary" icon="plus" @click="doAdd"></Button>
                   </Tooltip>
-                  <Tooltip content="删除" placement="bottom-start">
-                  <Button type="error" icon="close"></Button>
-                  </Tooltip>
-              </ButtonGroup>
-            </div>
-            <div slot="main">
-              <Table highlight-row size="small" class="gztag" :columns="moduleColumns" :data="moduleData"></Table>
-            </div>
-            <!-- <div slot="bottom">这里是底部</div> -->
+                  <Tooltip content="修改" placement="bottom-start" >
+                    <Button type="success" icon="edit"></Button>
+                    </Tooltip>
+                    <Tooltip content="删除" placement="bottom-start">
+                    <Button type="error" icon="close"></Button>
+                    </Tooltip>
+                </ButtonGroup>
+              </div>
+              <div slot="main">
+                <Table highlight-row size="small" class="gztag" :columns="moduleColumns" :data="moduleData"></Table>
+              </div>
+          </gz-panel>
+        </div>
         </gz-panel>
-      </div>
-      
-      </gz-panel>
+    </Col>
+      <Col span="18" class="fill-height">
+      <gz-panel class="borderPanel" >
+        <div slot="top" class="title">
+            <span>功能列表</span>
+        </div>
+        <div slot="main">
+          <gz-panel ref="gzPanelForm" :childFull100=true >
+              <div slot="top" class="tools" >    
+                  <gz-button ref="refresh" icon="refresh" @click="doRresh" :showText='true' text="刷新" loadingText="刷新中" />
+                <ButtonGroup>
+                    <Button type="primary" icon="plus" @click="doAdd">新增</Button>
+                    <Button type="success" icon="edit">修改</Button>
+                    <Button type="error" icon="close">删除</Button>
+                </ButtonGroup>
+              </div>
+              <div slot="main">
+                <Table highlight-row size="small" class="gztag" :columns="formColumns" :data="formData"></Table>
+              </div>
+          </gz-panel>
+        </div>
+        </gz-panel>
     </Col>
   </Row>
     
@@ -60,54 +80,56 @@
 <style lang="less" scoped>
 .tools {
   padding-top: 5px;
+  padding-left: 5px;
+  padding-bottom: 5px;
 }
-.title{
-    height: 32px;
-    line-height: 32px;
-    border-bottom: 1px solid #e9eaec!important;
-    color: #495060!important;
-    background: #fff!important;
-    padding: 0 12px;
-    display: inline-block;
-    margin: 2px 4px 2px 0;
-    border-radius: 3px;
-    font-size: 12px;
-    vertical-align: middle;
-    opacity: 1;
-    overflow: hidden;
-    width: 100%;
-    font-weight: 700;
-    text-overflow: ellipsis;
-    white-space: nowrap;
+.title {
+  height: 32px;
+  line-height: 32px;
+  border-bottom: 1px solid #e9eaec !important;
+  color: #495060 !important;
+  background: #fff !important;
+  padding: 0 12px;
+  display: inline-block;
+  margin: 2px 4px 2px 0;
+  border-radius: 3px;
+  font-size: 12px;
+  vertical-align: middle;
+  opacity: 1;
+  overflow: hidden;
+  width: 100%;
+  font-weight: 700;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 }
-.ivu-tooltip, .ivu-tooltip-rel {
-    // display: inline-block;
-    position: relative;
-    float: left;
-    border-radius: 0;
+.ivu-tooltip,
+.ivu-tooltip-rel {
+  // display: inline-block;
+  position: relative;
+  float: left;
+  border-radius: 0;
 }
 
-.tooltipButtonGroup{
+.tooltipButtonGroup {
   margin-left: 5px;
 }
-.tooltipButtonGroup>.ivu-tooltip {
+.tooltipButtonGroup > .ivu-tooltip {
   margin-left: -1px;
 }
-.tooltipButtonGroup>.ivu-tooltip:first-child:not(:last-child) .ivu-btn {
-    border-bottom-right-radius: 0;
-    border-top-right-radius: 0;
+.tooltipButtonGroup > .ivu-tooltip:first-child:not(:last-child) .ivu-btn {
+  border-bottom-right-radius: 0;
+  border-top-right-radius: 0;
 }
-.tooltipButtonGroup>.ivu-tooltip:last-child:not(:first-child) .ivu-btn {
-    border-bottom-left-radius: 0;
-    border-top-left-radius: 0;
+.tooltipButtonGroup > .ivu-tooltip:last-child:not(:first-child) .ivu-btn {
+  border-bottom-left-radius: 0;
+  border-top-left-radius: 0;
 }
 .tooltipButtonGroup .ivu-tooltip:not(:first-child):not(:last-child) .ivu-btn {
-    border-radius: 0;
+  border-radius: 0;
 }
-.borderPanel{
-  border: 1px solid #e9eaec!important;
+.borderPanel {
+  border: 1px solid #e9eaec !important;
 }
-
 </style>
 <script>
 import expandRow from "./table-expand.vue";
@@ -123,24 +145,60 @@ export default {
           type: "index",
           width: 50,
           align: "center",
-          className:"table-cell-index"
-        },{
+          className: "table-cell-index"
+        },
+        {
           title: "描述",
           key: "description",
           editable: true
         }
       ],
+      formColumns: [
+        {
+          title: "序号",
+          type: "index",
+          width: 60,
+          align: "center"
+        },
+        {
+          title: "名称",
+          key: "name",
+          editable: true
+        },
+        {
+          title: "描述",
+          key: "description",
+          editable: true
+        },
+        {
+          title: "图标",
+          key: "icon",
+          editable: true
+        },
+        {
+          title: "组件路径",
+          key: "componentPath",
+          editable: true
+        },
+        {
+          title: "组件名称",
+          key: "componentName",
+          editable: true
+        }
+      ],
       moduleData: [],
+      formData: [],
       moduleAdd: false
     };
   },
   created() {},
   mounted() {
     this.$refs.refresh.handleClick();
-    var me=this;
-    window.onresize=function(e){
-      me.$refs.gzabc.reLayout();
-    }
+    var me = this;
+    window.onresize = function(e) {
+      me.$refs.gzPanelModule.reLayout();
+      me.$refs.gzPanelForm.reLayout();
+    };
   },
   methods: {
     doRresh(event, component) {
@@ -149,6 +207,7 @@ export default {
         .get("/module/getall")
         .then(res => {
           this.moduleData = res.data;
+          this.formData = res.data;
           component.loading = false;
         })
         .catch(err => {
